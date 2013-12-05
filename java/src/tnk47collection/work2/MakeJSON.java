29cf894bb67ba876751d027ecc8f8560e6fcfc7f
wdxzs1985 <wdxzs1985@gmail.com>
2013-12-05 21:54:10 +0900

    add special card

--- java/src/tnk47collection/work2/MakeJSON.java
+++ java/src/tnk47collection/work2/MakeJSON.java
@@ -17,138 +17,140 @@ import org.apache.commons.lang3.StringUtils;
 
 public class MakeJSON implements Runnable {
 
-	public static final String INPUT_DIR = "data2/step3";
-	public static final String OUTPUT = "data2/step4/card.json";
+    public static final String INPUT_DIR = "data2/step3";
+    public static final String OUTPUT = "data2/step4/card.json";
 
-	public static void main(String[] args) {
-		new MakeJSON().run();
-	}
+    public static void main(final String[] args) {
+        new MakeJSON().run();
+    }
 
-	@Override
-	public void run() {
-		Map<String, String> regionMap = this.getRegionMap();
+    @Override
+    public void run() {
+        final Map<String, String> regionMap = this.getRegionMap();
 
-		System.out.printf("%s start\n", this.getClass().getSimpleName());
+        System.out.printf("%s start\n", this.getClass().getSimpleName());
 
-		Set<String> mergeLines = new HashSet<>();
+        final Set<String> mergeLines = new HashSet<String>();
 
-		try {
-			Collection<File> inputFiles = FileUtils.listFiles(new File(
-					INPUT_DIR), FileFileFilter.FILE, null);
-			for (File input : inputFiles) {
-				List<String> inputLines = FileUtils.readLines(input);
-				mergeLines.addAll(inputLines);
-			}
+        try {
+            final Collection<File> inputFiles = FileUtils.listFiles(new File(MakeJSON.INPUT_DIR),
+                                                                    FileFileFilter.FILE,
+                                                                    null);
+            for (final File input : inputFiles) {
+                final List<String> inputLines = FileUtils.readLines(input);
+                mergeLines.addAll(inputLines);
+            }
 
-			Map<String, JSONObject> mapping = new HashMap<>();
-			for (String line : mergeLines) {
-				String[] prop = StringUtils.splitPreserveAllTokens(line, ",");
-				String ill = prop[0];
-				String pref = prop[1];
-				String region = regionMap.get(prop[1]);
-				String rarilites = prop[2];
-				String name = prop[3];
-				rarilites = StringUtils.replace(rarilites, "ssrare", "SSR");
-				rarilites = StringUtils.replace(rarilites, "srare", "SR");
-				rarilites = StringUtils.replace(rarilites, "hrare", "HR");
+            final Map<String, JSONObject> mapping = new HashMap<String, JSONObject>();
+            for (final String line : mergeLines) {
+                final String[] prop = StringUtils.splitPreserveAllTokens(line,
+                                                                         ",");
+                final String ill = prop[0];
+                final String pref = prop[1];
+                final String region = regionMap.get(prop[1]);
+                String rarilites = prop[2];
+                final String name = prop[3];
+                rarilites = StringUtils.replace(rarilites, "ssrare", "SSR");
+                rarilites = StringUtils.replace(rarilites, "srare", "SR");
+                rarilites = StringUtils.replace(rarilites, "hrare", "HR");
 
-				JSONObject card = null;
-				if (mapping.containsKey(name)) {
-					card = mapping.get(name);
-					JSONArray illList = card.getJSONArray("ill");
-					illList.add(ill);
-				} else {
-					card = new JSONObject();
-					card.put("name", name);
-					card.put("rarilites", rarilites);
-					card.put("region", StringUtils.defaultIfBlank(region, "不明"));
-					card.put("pref", StringUtils.defaultIfBlank(pref, "不明"));
-					card.put("type", "不明");
+                JSONObject card = null;
+                if (mapping.containsKey(name)) {
+                    card = mapping.get(name);
+                    final JSONArray illList = card.getJSONArray("ill");
+                    illList.add(ill);
+                } else {
+                    card = new JSONObject();
+                    card.put("name", name);
+                    card.put("rarilites", rarilites);
+                    card.put("region", StringUtils.defaultIfBlank(region, "不明"));
+                    card.put("pref", StringUtils.defaultIfBlank(pref, "不明"));
+                    card.put("type", "不明");
 
-					JSONArray illList = new JSONArray();
-					illList.add(ill);
-					card.put("ill", illList);
+                    final JSONArray illList = new JSONArray();
+                    illList.add(ill);
+                    card.put("ill", illList);
 
-					mapping.put(name, card);
-				}
-			}
+                    mapping.put(name, card);
+                }
+            }
 
-			JSONArray outputList = new JSONArray();
-			outputList.addAll(mapping.values());
+            final JSONArray outputList = new JSONArray();
+            outputList.addAll(mapping.values());
 
-			FileUtils.write(new File(OUTPUT),
-					"var cards = " + outputList.toString());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+            FileUtils.write(new File(MakeJSON.OUTPUT),
+                            "var cards = " + outputList.toString());
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
 
-		System.out.printf("%s end\n", this.getClass().getSimpleName());
-	}
+        System.out.printf("%s end\n", this.getClass().getSimpleName());
+    }
 
-	private Map<String, String> getRegionMap() {
-		Map<String, String> regionMap = new HashMap<String, String>();
-		regionMap.put("無所属", "無所属");
-		//
-		regionMap.put("北海道・東北", "北海道・東北");
-		regionMap.put("青森", "北海道・東北");
-		regionMap.put("岩手", "北海道・東北");
-		regionMap.put("北海道", "北海道・東北");
-		regionMap.put("秋田", "北海道・東北");
-		regionMap.put("山形", "北海道・東北");
-		regionMap.put("宮城", "北海道・東北");
-		regionMap.put("福島", "北海道・東北");
-		// 中部
-		regionMap.put("中部", "中部");
-		regionMap.put("愛知", "中部");
-		regionMap.put("中部", "中部");
-		regionMap.put("新潟", "中部");
-		regionMap.put("山梨", "中部");
-		regionMap.put("静岡", "中部");
-		regionMap.put("岐阜", "中部");
-		regionMap.put("長野", "中部");
-		regionMap.put("石川", "中部");
-		regionMap.put("富山", "中部");
-		// 中国・四国
-		regionMap.put("中国・四国", "中国・四国");
-		regionMap.put("山口", "中国・四国");
-		regionMap.put("徳島", "中国・四国");
-		regionMap.put("高知", "中国・四国");
-		regionMap.put("愛媛", "中国・四国");
-		regionMap.put("岡山", "中国・四国");
-		regionMap.put("愛媛", "中国・四国");
-		regionMap.put("鳥取", "中国・四国");
-		regionMap.put("香川", "中国・四国");
-		regionMap.put("広島", "中国・四国");
-		regionMap.put("島根", "中国・四国");
-		// 関東
-		regionMap.put("関東", "関東");
-		regionMap.put("栃木", "関東");
-		regionMap.put("群馬", "関東");
-		regionMap.put("茨城", "関東");
-		regionMap.put("埼玉", "関東");
-		regionMap.put("東京", "関東");
-		regionMap.put("神奈川", "関東");
-		// 近畿
-		regionMap.put("近畿", "近畿");
-		regionMap.put("奈良", "近畿");
-		regionMap.put("大阪", "近畿");
-		regionMap.put("京都", "近畿");
-		regionMap.put("滋賀", "近畿");
-		regionMap.put("福井", "近畿");
-		regionMap.put("三重", "近畿");
-		regionMap.put("和歌山", "近畿");
-		regionMap.put("兵庫", "近畿");
-		// 九州・沖縄
-		regionMap.put("九州・沖縄", "九州・沖縄");
-		regionMap.put("熊本", "九州・沖縄");
-		regionMap.put("九州", "九州・沖縄");
-		regionMap.put("佐賀", "九州・沖縄");
-		regionMap.put("長崎", "九州・沖縄");
-		regionMap.put("鹿児島", "九州・沖縄");
-		regionMap.put("宮崎", "九州・沖縄");
-		regionMap.put("大分", "九州・沖縄");
-		//
+    private Map<String, String> getRegionMap() {
+        final Map<String, String> regionMap = new HashMap<String, String>();
+        regionMap.put("無所属", "無所属");
+        //
+        regionMap.put("北海道・東北", "北海道・東北");
+        regionMap.put("青森", "北海道・東北");
+        regionMap.put("岩手", "北海道・東北");
+        regionMap.put("北海道", "北海道・東北");
+        regionMap.put("秋田", "北海道・東北");
+        regionMap.put("山形", "北海道・東北");
+        regionMap.put("宮城", "北海道・東北");
+        regionMap.put("福島", "北海道・東北");
+        // 中部
+        regionMap.put("中部", "中部");
+        regionMap.put("愛知", "中部");
+        regionMap.put("中部", "中部");
+        regionMap.put("新潟", "中部");
+        regionMap.put("山梨", "中部");
+        regionMap.put("静岡", "中部");
+        regionMap.put("岐阜", "中部");
+        regionMap.put("長野", "中部");
+        regionMap.put("石川", "中部");
+        regionMap.put("富山", "中部");
+        // 中国・四国
+        regionMap.put("中国・四国", "中国・四国");
+        regionMap.put("山口", "中国・四国");
+        regionMap.put("徳島", "中国・四国");
+        regionMap.put("高知", "中国・四国");
+        regionMap.put("愛媛", "中国・四国");
+        regionMap.put("岡山", "中国・四国");
+        regionMap.put("愛媛", "中国・四国");
+        regionMap.put("鳥取", "中国・四国");
+        regionMap.put("香川", "中国・四国");
+        regionMap.put("広島", "中国・四国");
+        regionMap.put("島根", "中国・四国");
+        // 関東
+        regionMap.put("関東", "関東");
+        regionMap.put("栃木", "関東");
+        regionMap.put("群馬", "関東");
+        regionMap.put("茨城", "関東");
+        regionMap.put("埼玉", "関東");
+        regionMap.put("東京", "関東");
+        regionMap.put("神奈川", "関東");
+        // 近畿
+        regionMap.put("近畿", "近畿");
+        regionMap.put("奈良", "近畿");
+        regionMap.put("大阪", "近畿");
+        regionMap.put("京都", "近畿");
+        regionMap.put("滋賀", "近畿");
+        regionMap.put("福井", "近畿");
+        regionMap.put("三重", "近畿");
+        regionMap.put("和歌山", "近畿");
+        regionMap.put("兵庫", "近畿");
+        // 九州・沖縄
+        regionMap.put("九州・沖縄", "九州・沖縄");
+        regionMap.put("熊本", "九州・沖縄");
+        regionMap.put("九州", "九州・沖縄");
+        regionMap.put("佐賀", "九州・沖縄");
+        regionMap.put("長崎", "九州・沖縄");
+        regionMap.put("鹿児島", "九州・沖縄");
+        regionMap.put("宮崎", "九州・沖縄");
+        regionMap.put("大分", "九州・沖縄");
+        //
 
-		return regionMap;
-	}
+        return regionMap;
+    }
 }
--- java/src/tnk47collection/work2/MakeRawText.java
+++ java/src/tnk47collection/work2/MakeRawText.java
@@ -13,63 +13,60 @@ import org.apache.commons.io.FileUtils;
 
 public class MakeRawText implements Runnable {
 
-	static final ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors
-			.newFixedThreadPool(10);
+    static final ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(10);
 
-	public static final Pattern TAG_IMG = Pattern
-			.compile("<img src=\"http://stat100.ameba.jp/tnk47/ratio10/illustrations/card/(.*?)\">");
-	public static final Pattern TAG_IMG2 = Pattern
-			.compile("(ill_\\d+_[a-z_\\-]+\\d{2})");
-	public static final Pattern TAG_REGION = Pattern
-			.compile("<span class=\"regionBackgroundColor .*?\">(.*?)</span>");
-	public static final Pattern TAG_NAME = Pattern
-			.compile("<dd><span class=\"cardRarity (.*?)\"></span>(.*?)</dd>");
+    public static final Pattern TAG_IMG = Pattern.compile("<img src=\"http://stat100.ameba.jp/tnk47/ratio10/illustrations/card/(.*?)\">");
+    public static final Pattern TAG_IMG2 = Pattern.compile("(ill_\\d+_[a-z_\\-]+\\d{2})");
+    public static final Pattern TAG_REGION = Pattern.compile("<span class=\"regionBackgroundColor .*?\">(.*?)</span>");
+    public static final Pattern TAG_NAME = Pattern.compile("<dd><span class=\"cardRarity (.*?)\"></span>(.*?)</dd>");
 
-	private final int number;
+    private final int number;
 
-	public static void main(String[] args) {
-		for (int i = 1; i < 7880; i++) {
-			executor.execute(new MakeRawText(i));
-		}
-		executor.shutdown();
-	}
+    public static void main(final String[] args) {
+        for (int i = 1; i < 7880; i++) {
+            MakeRawText.executor.execute(new MakeRawText(i));
+        }
+        MakeRawText.executor.shutdown();
+    }
 
-	public MakeRawText(int number) {
-		this.number = number;
-	}
+    public MakeRawText(final int number) {
+        this.number = number;
+    }
 
-	@Override
-	public void run() {
-		System.out.printf("%s(%d) start\n", this.getClass().getSimpleName(),
-				this.number);
-		String input = String.format("data2/step1/%d.html", this.number);
-		String output = String.format("data2/step2/%d.txt", this.number);
+    @Override
+    public void run() {
+        System.out.printf("%s(%d) start\n",
+                          this.getClass().getSimpleName(),
+                          this.number);
+        final String input = String.format("data2/step1/%d.html", this.number);
+        final String output = String.format("data2/step2/%d.txt", this.number);
 
-		try {
-			List<String> inputLines = FileUtils.readLines(new File(input));
-			List<String> outputLines = new ArrayList<>();
-			for (String line : inputLines) {
-				String found = line;
-				Matcher matcher = null;
-				if ((matcher = TAG_IMG.matcher(found)).find()) {
-					found = matcher.group(1);
-					if ((matcher = TAG_IMG2.matcher(found)).find()) {
-						outputLines.add(matcher.group(1));
-					}
-				} else if ((matcher = TAG_REGION.matcher(found)).find()) {
-					outputLines.add(matcher.group(1));
-				} else if ((matcher = TAG_NAME.matcher(found)).find()) {
-					outputLines.add(matcher.group(1));
-					outputLines.add(matcher.group(2));
-				}
-			}
-			FileUtils.writeLines(new File(output), outputLines);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+        try {
+            final List<String> inputLines = FileUtils.readLines(new File(input));
+            final List<String> outputLines = new ArrayList<String>();
+            for (final String line : inputLines) {
+                String found = line;
+                Matcher matcher = null;
+                if ((matcher = MakeRawText.TAG_IMG.matcher(found)).find()) {
+                    found = matcher.group(1);
+                    if ((matcher = MakeRawText.TAG_IMG2.matcher(found)).find()) {
+                        outputLines.add(matcher.group(1));
+                    }
+                } else if ((matcher = MakeRawText.TAG_REGION.matcher(found)).find()) {
+                    outputLines.add(matcher.group(1));
+                } else if ((matcher = MakeRawText.TAG_NAME.matcher(found)).find()) {
+                    outputLines.add(matcher.group(1));
+                    outputLines.add(matcher.group(2));
+                }
+            }
+            FileUtils.writeLines(new File(output), outputLines);
+        } catch (final IOException e) {
+            e.printStackTrace();
+        }
 
-		System.out.printf("%s(%d) end\n", this.getClass().getSimpleName(),
-				this.number);
-	}
+        System.out.printf("%s(%d) end\n",
+                          this.getClass().getSimpleName(),
+                          this.number);
+    }
 
 }
